b93b890eff1337749719f63fb03a16a979ba29e7
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-02-25 21:39:54 +0800

    fix: SignOut button w-full h-full

--- src/app/_components/TopBar.tsx
+++ src/app/_components/TopBar.tsx
@@ -10,22 +10,22 @@ import {
 } from "~/components/ui/dropdown-menu";
 import { TbTriangleInvertedFilled } from "react-icons/tb";
 
-export default async function TopBar({createMode}:{createMode?: boolean}) {
+export default async function TopBar({ createMode }: { createMode?: boolean }) {
   const session = await getServerAuthSession();
 
   return (
     <div className="flex h-full w-full justify-center">
-      <div className="flex pl-2 h-full items-center gap-2">
+      <div className="flex h-full items-center gap-2 pl-2">
         <span className="flex h-full items-center text-3xl text-white">
           <FaBlogger />
         </span>
         <Link
           href={"/"}
-          className="font-mono text-xl font-bold text-white flex-shrink-0"
+          className="flex-shrink-0 font-mono text-xl font-bold text-white"
         >{`丹尼爾的部落格`}</Link>
-        <div className="mx-4 flex h-full items-center py-2 flex-shrink">
+        <div className="mx-4 flex h-full flex-shrink items-center py-2">
           <input
-            className="h-full w-96 min-w-0 rounded-md flex-shrink rounded-r-none border-sky-800 bg-sky-800 px-2 text-sm text-white outline-none"
+            className="h-full w-96 min-w-0 flex-shrink rounded-md rounded-r-none border-sky-800 bg-sky-800 px-2 text-sm text-white outline-none"
             placeholder="搜尋文章"
             type="text"
           />
@@ -42,27 +42,29 @@ export default async function TopBar({createMode}:{createMode?: boolean}) {
           </Link>
         )}
         {session && (
-        <>
+          <>
             <Link
-                href={"/create"}
-                className={`h-full flex items-center text-white ${createMode ?"bg-sky-800" :"hover:bg-sky-800"} px-6`}
-                >
-                <FaPen className="text-[16px]" />
+              href={"/create"}
+              className={`flex h-full items-center text-white ${createMode ? "bg-sky-800" : "hover:bg-sky-800"} px-6`}
+            >
+              <FaPen className="text-[16px]" />
             </Link>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <div className="flex h-full cursor-pointer items-center px-4 gap-4 font-mono text-sm text-white">
-                <span>Hello, {session.user.name} </span>
-                <TbTriangleInvertedFilled className="cursor-pointer text-white" />
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <Link href={"/api/auth/signout"}>登出</Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <div className="flex h-full cursor-pointer items-center gap-4 px-4 font-mono text-sm text-white">
+                  <span>Hello, {session.user.name} </span>
+                  <TbTriangleInvertedFilled className="cursor-pointer text-white" />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link className="h-full w-full" href={"/api/auth/signout"}>
+                    登出
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
         )}
       </div>
     </div>
