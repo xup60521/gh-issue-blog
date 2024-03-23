57fe820c913c91c7f548a415df163ad9d1d3a701
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-03-23 20:08:17 +0800

    fix: app/page redirect made vercel deployment page dead

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
+import { FaHome } from "react-icons/fa";
 import { IoMdDocument } from "react-icons/io";
 
 const NavBar: React.FC = async () => {
-
   return (
-    <nav className="text-white w-[208px] flex flex-col px-4 justify-left">
-      <Link href={"/post"} className="w-full flex gap-2 items-center hover:bg-[#0b2031] py-[0.625rem] pl-5 pr-8"><IoMdDocument className="text-[20px]" />文章列表</Link> 
-    </nav>
-  )
-}
+    <nav className="justify-left flex w-[208px] flex-col px-4 text-white">
+      <Link
+        href={"/"}
+        className="flex w-full items-center gap-2 py-[0.625rem] pl-5 pr-8 hover:bg-[#0b2031]"
+      >
+        <FaHome className="text-[20px]" />
+        主畫面
+      </Link>
 
-export default NavBar
+      <Link
+        href={"/post"}
+        className="flex w-full items-center gap-2 py-[0.625rem] pl-5 pr-8 hover:bg-[#0b2031]"
+      >
+        <IoMdDocument className="text-[20px]" />
+        文章列表
+      </Link>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import { redirect } from "next/navigation";
+import TopBar from "./_components/TopBar";
+import NavBar from "./_components/NavBar";
+import RightBar from "./_components/RightBar";
 
 export default async function Page() {
-  redirect("/post");
+    return (
+        <>
+          <div className={`absolute left-0 top-0 h-12 w-full flex-grow bg-sky-700`}>
+            <TopBar />
+          </div>
+          <div
+            className={`box-border flex h-screen w-full flex-grow justify-center bg-sky-950 pt-16`}
+          >
+            <NavBar />
+            <main className="h-[calc(100vh-4rem)] w-1/2 pb-0">
+              <div className="h-[calc(100vh-4rem)] w-full overflow-y-scroll rounded-md rounded-b-none bg-white p-2 pb-0">
+                <div className="box-border flex h-[calc(100vh-4rem)] w-full flex-col gap-3 p-2 px-8 pb-8 justify-center items-center">
+                  主畫面，目前沒想到要放什麼...
+                </div>
+              </div>
+            </main>
+            <RightBar />
+          </div>
+        </>
+      );
 }
